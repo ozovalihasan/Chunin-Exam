acf03edf9b000f618b51ccc4f84e935a7ed92f2c
Hasan Ozovali <ozovalihasan@gmail.com>
2021-04-28 13:03:00 +0300

    Add validation for url  attribute of Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,9 @@
 class Link < ApplicationRecord
   before_create :set_short_url
   validates :url, uniqueness: { case_sensitive: false }, presence: true
+  validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  private
 
   def set_short_url
     self.short_url = uniq_short_url
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:url) }
+    it { should allow_value('https://www.google.com/').for(:url) }
+    it { should_not allow_value('www.google.com/').for(:url) }
   end
 end
